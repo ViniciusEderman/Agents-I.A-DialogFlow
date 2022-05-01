0454befde60fb2527328302bc481f17282b6cbd1
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2022-05-01 16:32:01 -0300

    Adicionando body-parser

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const app = express();
 
+let bodyParser = require('body-parser')
+app.use( bodyParser.json() );
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
 app.use(express.static('public'));
 
 app.get('/', function(request, response){
     response.send("Teste");
 });
 
+app.post("/nomedoagente", function(request, response){
+
+    let intent 
+});
+
 var port =  process.env.PORT || 3000;
+
 const listener = app.listen(port, function() {
     console.log("Sua aplicação está na porta: " + listener.address().port);
 });
